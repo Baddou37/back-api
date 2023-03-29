874cb0efde37bbee7db4d77ebd9f00640a493f9f
Julien Thibault <julien.thibault@dentalviamedilor.com>
2023-03-29 17:22:14 +0200

    convertion

--- app.js
+++ app.js
@@ -4,31 +4,7 @@ const { exec } = require('child_process');
 const mime = require('mime-types');
 const fs = require('fs');
 
-const mimeMap = {
-  'png': 'image/png',
-  'jpg': 'image/jpeg',
-  'jpeg': 'image/jpeg',
-  'gif': 'image/gif',
-  'tiff': 'image/tiff',
-  'tif': 'image/tiff',
-  'bmp': 'image/bmp',
-  'heic': 'image/heic',
-  'heif': 'image/heif',
-  'dcm': 'image/dcm'
-}
-
-// get the list of files in the data folder
-const files = fs.readdirSync('data');
-console.log('before', files);
-// Ignore DS_Store & Desktop.ini if it exists in the folder using a loop
-for (let i = 0; i < files.length; i++) {
-  if (files[i] === '.DS_Store' || files[i] === 'desktop.ini') {
-    files.splice(i, 1);
-    i--;
-  }
-}
-console.log('after', files);
-
+// DICOM to JPEG
 const convertDicom = () => {
   exec('python3 -c "import pydicom; import matplotlib.pyplot as plt; dcm = pydicom.dcmread(\'data/img.dcm\'); image = dcm.pixel_array; plt.imsave(\'convert/testDICOM.jpg\', image, cmap=plt.cm.bone)"', (error, stdout, stderr) => {
     if (error) {
@@ -39,8 +15,8 @@ const convertDicom = () => {
     console.error(`stderr: ${stderr}`);
   });
 };
-convertDicom();
 
+// HEIC to JPEG
 const convertHeif = () => {
   exec('python3 -c "from PIL import Image; from pillow_heif import register_heif_opener; register_heif_opener(); im = Image.open(\'data/img.heic\'); im.convert(\'RGB\').save(\'convert/testHEIC.jpg\', \'JPEG\')"', (error, stdout, stderr) => {
     if (error) {
@@ -51,8 +27,8 @@ const convertHeif = () => {
     console.error(`stderr: ${stderr}`);
   });
 };
-convertHeif();
 
+// TIFF to JPEG
 const convertTiff = () => {
   exec('python3 -c "from PIL import Image; im = Image.open(\'data/img.tiff\'); im.convert(\'RGB\').save(\'convert/testTIFF.jpg\', \'JPEG\')"', (error, stdout, stderr) => {
     if (error) {
@@ -63,8 +39,36 @@ const convertTiff = () => {
     console.error(`stderr: ${stderr}`);
   });
 };
-convertTiff();
+
+// get the list of files in the data folder
+const files = fs.readdirSync('data');
+// Ignore DS_Store & Desktop.ini if it exists in the folder using a loop
+for (let i = 0; i < files.length; i++) {
+  if (files[i] === '.DS_Store' || files[i] === 'desktop.ini') {
+    files.splice(i, 1);
+    i--;
+  }
+}
+
+files.forEach(file => {
+  const extension = getExtension(file);
+  switch (extension) {
+    case 'dcm':
+      convertDicom();
+      break;
+    case 'heic':
+      convertHeif();
+      break;
+    case 'tiff':
+      convertTiff();
+      break;
+  }
+});
+
+function getExtension(filename) {
+  const i = filename.lastIndexOf('.');
+  return (i < 0) ? '' : filename.substr(i+1);
+}
 
 // Give an acces for server.js
 module.exports = app;
-
